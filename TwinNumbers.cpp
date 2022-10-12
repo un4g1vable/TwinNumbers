cfca0b28d582030a7ef2b108629c9d760e26ef55
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-10-13 01:27:42 +0500

    Add files via upload

---
+++ TwinNumbers.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int counter1 = 0, counter2 = 0;
+    int number1, div1, number2, div2;
+    cout << "Введите первое число: ";
+    cin >> number1;
+    cout << "Введите второе число: ";
+    cin >> number2;
+
+    for (div1 = number1; div1 > 0; div1--) {
+        if (number1 % div1 == 0) {
+            counter1 += 1;
+        }
+    }
+
+    for (div2 = number2; div2 > 0; div2--) {
+        if (number2 % div2 == 0) {
+            counter2 += 1;
+        }
+    }
+
+    if ((counter1 = counter2 == 2) and abs(number1 - number2) == 2)
+        cout << "Числа близнецы" << endl;
+    else
+        cout << "Числа не близнецы" << endl;
+}
